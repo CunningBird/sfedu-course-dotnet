bbc307668e3d24a276a65b0ade89b1d14bc9a652
CunningBird <68229355+CunningBird@users.noreply.github.com>
2021-11-28 20:35:58 +0300

    Create solution

---
+++ LaptevTask10-11/LaptevTask10/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace LaptevTask10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Task 10");
+        }
+    }
+}
---
+++ LaptevTask10-11/LaptevTask11/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace LaptevTask11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Task 11");
+        }
+    }
+}
