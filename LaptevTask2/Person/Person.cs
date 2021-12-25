9fa59d7e25ee1c7a951db8e4c7744fbefc3e86c1
CunningBird <68229355+CunningBird@users.noreply.github.com>
2021-12-25 22:04:00 +0300

    Complete task

---
+++ LaptevTask2/Person/Person.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+namespace LaptevTask2 {
+    public class Person {
+        private string Name {
+            get;
+        }
+        
+        private int age;
+
+        private int Age {
+            get {
+                return age;
+            }
+            set {
+                if (value < 0) value = 0;
+                age = value;
+            }
+        }
+
+        protected Person(string name, int age) {
+            Name = name;
+            Age = age;
+        }
+
+        public virtual void Print() {
+            Console.WriteLine(Name + " " + Age);
+        }
+
+        public override string ToString() => $"{Name}{Age}";
+
+        public override int GetHashCode() => Name.GetHashCode() ^ Age.GetHashCode();
+
+        public override bool Equals(object obj) {
+            if (obj == null) {
+                return false;
+            }
+            if (obj.GetType() != GetType()) {
+                return false;
+            }
+            var p = obj as Person;
+            return p.Name == Name && p.Age == Age;
+        }
+
+        public static void RandomPerson(Person[] per) {
+            Console.WriteLine(per[new Random().Next(0, per.Length)]);
+        }
+
+        public static bool operator ==(Person p1, Person p2) {
+            return p1.Equals(p2);
+        }
+
+        public static bool operator !=(Person p1, Person p2) {
+            return !(p1 == p2);
+        }
+    }
+}
---
+++ LaptevTask2/Teacher/Teacher.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace LaptevTask2 {
+    public class Teacher : Person {
+        string Department {
+            get;
+        }
+
+        public List<Student> Students {
+            get;
+            set;
+        }
+
+        public Teacher(string name, int age, string department, List<Student> students) : base(name, age) {
+            Department = department;
+            Students = students;
+        }
+
+        public override void Print() {
+            base.Print();
+            Console.WriteLine(Department);
+            for (int i = 0; i < Students.Count(); ++i) {
+                Students[i].Print();
+            }
+        }
+
+        public override string ToString() => base.ToString() + $"{Department}{Students}";
+
+        public override int GetHashCode() => base.GetHashCode() ^ Department.GetHashCode() ^ Students.GetHashCode();
+
+        public override bool Equals(object obj) {
+            var t = obj as Teacher;
+            return base.Equals(obj) && t.Department == Department && t.Students == Students;
+        }
+
+        public static void RandomTeacher(Teacher[] teach) {
+            Console.WriteLine(teach[new Random().Next(0, teach.Length)]);
+        }
+
+        public static bool operator ==(Teacher t1, Teacher t2) {
+            return t1.Equals(t2);
+        }
+
+        public static bool operator !=(Teacher t1, Teacher t2) {
+            return !(t1 == t2);
+        }
+    }
+}
