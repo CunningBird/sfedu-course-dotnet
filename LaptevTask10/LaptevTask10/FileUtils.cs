f16109a6dfe7ca760ded9c1c66ccf7cb0a60f49f
CunningBird <68229355+CunningBird@users.noreply.github.com>
2021-12-25 12:04:31 +0300

    Complete Task

---
+++ LaptevTask10/LaptevTask10/FileUtils.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace LaptevTask10
+{
+    public class FileUtils {
+        public static void ReadingPrint(string filename, Encoding encoder) {
+            using (var reader = new StreamReader(filename, encoder)) {
+                while (!reader.EndOfStream) Console.WriteLine(reader.ReadLine());
+            }
+
+            Console.WriteLine();
+        }
+
+        public static void Single_byteEncodings(string txt) {
+            File.WriteAllText("dos.txt", "MS DOS:" + txt, Encoding.GetEncoding("ibm850"));
+            ReadingPrint("dos.txt", Encoding.GetEncoding("ibm850"));
+
+            File.WriteAllText("koi8.txt", "koi8:" + txt, Encoding.GetEncoding("koi8-r"));
+            ReadingPrint("koi8.txt", Encoding.GetEncoding("koi8-r"));
+
+            File.WriteAllText("cp1251.txt", "1251:" + txt, Encoding.GetEncoding(1251));
+            ReadingPrint("cp1251.txt", Encoding.GetEncoding(1251));
+        }
+
+        public static void UnicodeEncodings(string txt) {
+            File.WriteAllText("utf8.txt", "utf8:" + txt, Encoding.UTF8);
+            ReadingPrint("utf8.txt", Encoding.UTF8);
+
+            File.WriteAllText("utf16.txt", "utf16:" + txt, Encoding.GetEncoding("Utf-16"));
+            ReadingPrint("utf16.txt", Encoding.GetEncoding("Utf-16"));
+        }
+
+
+        public static void DeleteInFile(string txt, int k) {
+            File.WriteAllLines("testt.txt", File.ReadAllLines(txt).Reverse().Skip(k).Reverse());
+            Console.WriteLine("Delete 5 strings in end of testt.txt are successful");
+        }
+
+
+        public static void SumInFile(string txt) {
+            var s = File.ReadAllLines(txt)
+                .Select(a => Regex.Replace(a, @"\s+", " ").Replace(@"^\s", "").Split(' ').Select
+                    (x => {
+                        try {
+                            var k = double.Parse(x);
+                            return k;
+                        }catch (Exception e) {
+                            return 0;
+                        }
+                    }).Sum()).Sum();
+            
+            Console.WriteLine("Sum in " + txt + ": " + s);
+        }
+
+        public static void DataSqare(string txt) {
+            var path = txt;
+            using (var fin = File.Create(path))
+            using (var binfin = new BinaryWriter(fin)) {
+                for (var i = 0; i < 10; i++) {
+                    binfin.Write(i);
+                }
+            }
+
+
+            using (var fin = File.Open(path, FileMode.Open))
+            using (var binfout = new BinaryReader(fin))
+            using (var binfin = new BinaryWriter(fin)) {
+                var len = fin.Length / sizeof(int);
+                for (var i = 0; i < len; i++) {
+                    var x = binfout.ReadInt32();
+                    binfin.Seek(-sizeof(int), SeekOrigin.Current);
+                    binfin.Write(x * x);
+                }
+            }
+
+
+            using (var fin = File.OpenRead(path))
+            using (var binfout = new BinaryReader(fin)) {
+                var size = binfout.BaseStream.Length / sizeof(int);
+                for (var i = 0; i < size; i++) Console.Write(binfout.ReadInt32() + " ");
+
+                Console.WriteLine();
+            }
+        }
+
+        public static void CurrentDirectory(string dir, int k = 0) {
+            var currDir = dir.Split('\\').Last() + ":";
+            Console.WriteLine(currDir.PadLeft(currDir.Length + k * 4));
+
+            foreach (var ndir in Directory.GetDirectories(dir)) CurrentDirectory(ndir, k + 1);
+
+            foreach (var file in Directory.GetFiles(dir, "*")) {
+                var fname = file.Replace(dir + "\\", "");
+                Console.WriteLine(fname.PadLeft(fname.Length + (k + 1) * 4));
+            }
+        }
+    }
+}
---
+++ LaptevTask10/LaptevTask10/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.IO;
+using static LaptevTask10.FileUtils;
+
+namespace LaptevTask10
+{
+    class Program {
+        private static void Main(string[] args) {
+            var text =
+                "Невероятно прекрасный, лаконичный и, что самое главное, полезный текст, который, к тому же, " +
+                "разбит на две строки, и содержит английские слова, такие как Three, hundred и bucks";
+
+            task10_1a(text);
+
+            task10_1b(text);
+
+            task10_2();
+
+            task10_3();
+
+            task10_4();
+
+            task10_5();
+        }
+
+        private static void task10_1a(string text) {
+            Console.WriteLine(" Single_byte Encodings");
+            Single_byteEncodings(text);
+            Console.WriteLine();
+        }
+
+        private static void task10_1b(string text) {
+            Console.WriteLine(" Unicode Ecodings");
+            UnicodeEncodings(text);
+            Console.WriteLine();
+        }
+
+        private static void task10_2() {
+            Console.WriteLine(" Delete In File");
+            DeleteInFile("test.txt", 5);
+            Console.WriteLine();
+        }
+
+        private static void task10_3() {
+            Console.WriteLine(" Sum In File");
+            SumInFile("numbers.txt");
+            Console.WriteLine();
+        }
+
+        private static void task10_4() {
+            Console.WriteLine(" Folders and files in current directory");
+            DataSqare("numbers.dat");
+            Console.WriteLine();
+        }
+
+        private static void task10_5() {
+            Console.WriteLine(" Mod2 Numbers");
+            CurrentDirectory(Directory.GetCurrentDirectory());
+            Console.WriteLine();
+
+            Console.ReadLine();
+        }
+    }
+}
